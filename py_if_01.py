6db80371b886271606ea8f56f7acb1bf6aae0348
Jašo <andrija.pericic@gmail.com>
2025-09-09 20:39:08 +0200

    Create py_if_01.py

---
+++ py_if_01.py
@@ -0,0 +1,18 @@
+print()
+for number in range(1, 31):
+    if number % 3 == 0:
+        print(f'Broj {number} JE djeljiv sa 3')
+    if number % 6 == 0:
+        print(f'Broj {number} JE djeljiv sa 6')
+    if number % 9 == 0:
+        print(f'Broj {number} JE djeljiv sa 9')
+    else:
+        print(f'Broj {number} NIJE djeljiv sa 3,6 ili 9')
+
+
+
+
+
+
+
+
