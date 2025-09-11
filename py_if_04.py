08edd4aeb39e293e7f5bc4096907849f58588ee3
Jašo <andrija.pericic@gmail.com>
2025-09-11 18:52:35 +0200

    Update py_if_04.py

--- py_if_04.py
+++ py_if_04.py
@@ -18,7 +18,7 @@ Znak je *
 '''
 
 
-height = int(input('Upisi visinu: '))
+'''height = int(input('Upisi visinu: '))
 symbol = input('Unesi simbol: ')
 
 for i in range(height):
@@ -31,5 +31,24 @@ deblo_sirina = 3
 deblo_visina = 2
 
 for j in range(deblo_visina):
-    razmaci_deblo = ' ' * (height - 2)
-    
+    razmaci_deblo = ' ' * (height - 2)'''
+
+height = 10
+symbol = '*'
+
+# height = int(input('Upisite visinu jelke: '))
+# symbol = input('Upisite znak: ')
+
+for i in range(1, height + 1):       # 0 - 10
+    # Ispis praznina
+    print(' ' * (height - i), end='')
+
+    # Ispis zvjezdica
+    #print(symbol * ((i * 2) -1))
+    for j in range(1, i):
+        print(symbol * (j - 1), end='')
+          
+
+for j in range(3):
+    print(' ' * (height - 2), end='')
+    print('|' * 3)
